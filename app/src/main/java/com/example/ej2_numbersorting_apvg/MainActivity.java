4b9a735412519a6842563a754d531e71e4594252
Alan V <alan.varela.garcia@gmail.com>
2019-11-10 22:23:24 -0600

    Se añade procedimiento bubble sort Se requiere revisar lógica para el manejo del algoritmo

--- app/src/main/java/com/example/ej2_numbersorting_apvg/MainActivity.java
+++ app/src/main/java/com/example/ej2_numbersorting_apvg/MainActivity.java
@@ -7,7 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+import java.util.Arrays;
+import java.util.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +16,8 @@ public class MainActivity extends AppCompatActivity {
     Button submitBttn;
     EditText editTextNumber;
 
-    public static List<Integer> numberArray = new ArrayList<>();
+    public static List<Integer> numberList = new ArrayList<>();
+    public static List<Integer> auxList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,12 +31,35 @@ public class MainActivity extends AppCompatActivity {
         submitBttn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                numberArray.add(Integer.parseInt(editTextNumber.getText().toString()));
-                Toast.makeText(MainActivity.this, numberArray.toString(), Toast.LENGTH_SHORT).show();
+                if (numberList.size()<20) {
+                    numberList.add(Integer.parseInt(editTextNumber.getText().toString()));
+                    Toast.makeText(MainActivity.this, numberList.toString(), Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Sólo puedes ingresar 20 números", Toast.LENGTH_SHORT).show();
+                }
 
-
+                Integer numberArray[] = numberList.toArray(new Integer[numberList.size()]);//Conversión de lista a arreglo
+                bubbleSort(numberArray);
+                Collections.addAll(auxList,numberArray);
+                Toast.makeText(MainActivity.this, auxList.toString(), Toast.LENGTH_SHORT).show();
             }
         });
 
+
+
     }
+    public void bubbleSort(Integer arr[]){
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+    }
+
 }
